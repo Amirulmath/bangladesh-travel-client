7e3aaa48b177771f02d44f6efe4e952305c3a2b8
Amirulmath <amirulmath760@gmail.com>
2022-11-10 02:40:35 +0600

    Navbar & logo add

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../../../assets/images/Logo.png'
 
 const Header = () => {
     return (
-        <div>
-            <h3>Header</h3>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand>
+                    <Image style={{ height: '40px' }} src={Logo}></Image>
+                    <b>  Bangladesh Travel</b>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#features">Home</Nav.Link>
+                    <Nav.Link href="#pricing">Review</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="#deets">Login</Nav.Link>
+                    <Nav.Link eventKey={2} href="#memes">
+                    Register
+                    </Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
