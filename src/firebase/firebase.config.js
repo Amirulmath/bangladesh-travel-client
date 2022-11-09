d7ef7c6121f640a6c66a728accc8ed0999ba8978
Amirulmath <amirulmath760@gmail.com>
2022-11-10 01:28:40 +0600

    Web page title and favicon add

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7oOb_w4BLX4VTW6JXpBV-uS5b2iCR1qg",
+  authDomain: "bangladesh-travel-768aa.firebaseapp.com",
+  projectId: "bangladesh-travel-768aa",
+  storageBucket: "bangladesh-travel-768aa.appspot.com",
+  messagingSenderId: "536519003540",
+  appId: "1:536519003540:web:9e7b9f0129f6388d191381"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
