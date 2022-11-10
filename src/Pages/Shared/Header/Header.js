6a74f565939b05767ae254dffe10b4c01d157402
Amirulmath <amirulmath760@gmail.com>
2022-11-10 20:21:15 +0600

    Login Add

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Image } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import Logo from '../../../assets/images/Logo.png'
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
@@ -21,9 +32,20 @@ const Header = () => {
                     <Nav.Link href="#pricing">Review</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Nav.Link href="/login">Login</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                    Register
+                    <Nav.Link>
+                    {
+                        user?.uid ?
+                            <>
+                             <span>{user?.displayName}</span>
+                              <Button variant="light">My Review</Button>
+                              <Button variant="light">Add Service</Button>
+                              <Button variant="light" onClick={handleLogOut}>Log out</Button>
+                            </>
+                             :
+                              <>
+                             <Link to='/login'>Login</Link>
+                             </>
+                        }
                     </Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
