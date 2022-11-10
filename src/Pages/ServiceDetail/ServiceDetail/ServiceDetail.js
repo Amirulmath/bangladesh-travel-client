358e577184e318275a0727c0069e684eaaf3d2d5
Amirulmath <amirulmath760@gmail.com>
2022-11-10 17:58:30 +0600

    Arrange home & detail page

--- src/Pages/ServiceDetail/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail/ServiceDetail.js
@@ -5,16 +5,27 @@ import { Link } from 'react-router-dom';
 import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetail = () => {
-    const {title, image_url} = useLoaderData();
+    const {_id, title, details, fee, date, image_url} = useLoaderData();
     return (
-        <Card className="text-center">
+        <Card className="m-4 text-center">
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Img className="mb-2" variant="top" src={image_url} />
+                <Card.Text>
+                    <p><b>Course Detail: </b>{details}</p>
+                    <p><b>Travel Date: </b>{date}</p>
+                    <p><b>Every Person Cost: </b>Tk.{fee}</p>
+                </Card.Text>
             </Card.Body>
-            <Card.Footer className="text-muted">               
+            <Card.Footer className="d-flex justify-content-between text-muted">               
                 <Link>
-                    <Button variant="primary">Detail</Button>
+                    <Button variant="primary">Review</Button>
+                </Link>
+                <Link>
+                    <Button variant="primary">Add service</Button>
+                </Link>
+                <Link to='/'>
+                    <Button variant="primary">Back To Service</Button>
                 </Link>
             </Card.Footer>
         </Card>
--- src/Pages/Shared/ServiceCard/ServiceCard.js
+++ src/Pages/Shared/ServiceCard/ServiceCard.js
@@ -4,15 +4,23 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
 const ServiceCard = ({service}) => {
-    const { _id, title, image_url } = service;
+    const { _id, title, details, fee, date, image_url } = service;
 
     return (
-        <Card className="text-center">
+        <Card className=' text-center'>
             <Card.Body>
-                <Card.Title>{service.title}</Card.Title>
+                <Card.Title>{title}</Card.Title>
                 <Card.Img className="mb-2" variant="top" src={image_url} />
+                <Card.Text>
+                    <p><b>Course Detail: </b>{details.length > 100 ? details.slice(0,100) + '...' : details}</p>
+                    <p><b>Travel Date: </b>{date}</p>
+                    <p><b>Every Person Cost: </b>Tk.{fee}</p>
+                </Card.Text>
             </Card.Body>
-            <Card.Footer className="text-muted">               
+            <Card.Footer className="d-flex justify-content-between text-muted">            
+                <Link>
+                    <Button variant="primary">Review</Button>
+                </Link>
                 <Link to={`/detail/${_id}`}>
                     <Button variant="primary">Detail</Button>
                 </Link>
